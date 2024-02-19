59fdbe5e104de7c16955f69bfdb1f324ec912495
Enrique <enriquelopezgordillo@gmail.com>
2024-02-19 09:09:58 +0100

    Initial commit

---
+++ src/main/java/com/example/aplicacionvideojuegos/AplicacionVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.example.aplicacionvideojuegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionVideojuegosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicacionVideojuegosApplication.class, args);
+    }
+
+}
